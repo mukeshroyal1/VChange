5b675bda1820d53407df9127b5dd3dc76410dc4b
Meenbeese <meenbeese@tutanota.com>
2025-01-26 01:14:27 -0500

    Fix again

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { EventsContextProvider} from './context/EventContext'
-import { AuthContextProvider} from './context/AuthContext'
+import './frontend/src/index.css';
+import App from './frontend/src/App';
+import { EventsContextProvider} from './frontend/src/context/EventContext'
+import { AuthContextProvider} from './frontend/src/context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
